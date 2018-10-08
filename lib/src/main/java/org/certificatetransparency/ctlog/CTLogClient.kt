9ff78c42f904bbb4de1336f0983877d1719158ca
Matthew Dolan <info@mattdolan.com>
2018-10-08 13:45:37 +0100

    Convert CTLogClient to Kotlin

---
+++ lib/src/main/java/org/certificatetransparency/ctlog/CTLogClient.kt
@@ -0,0 +1,62 @@
+package org.certificatetransparency.ctlog
+
+import com.google.common.io.Files
+import org.certificatetransparency.ctlog.comm.HttpLogClient
+import org.certificatetransparency.ctlog.proto.Ct
+import org.certificatetransparency.ctlog.serialization.CryptoDataLoader
+import org.certificatetransparency.ctlog.serialization.Serializer
+
+import java.io.File
+import java.io.IOException
+import java.security.cert.Certificate
+
+/** The main CT log client. Currently only knows how to upload certificate chains to the ctlog.  */
+class CTLogClient(baseLogUrl: String, logInfo: LogInfo) {
+    private val httpClient: HttpLogClient = HttpLogClient(baseLogUrl)
+    private val signatureVerifier: LogSignatureVerifier = LogSignatureVerifier(logInfo)
+
+    /** Result of the certificate upload. Contains the SCT and verification result.  */
+    class UploadResult(val sct: Ct.SignedCertificateTimestamp, val isVerified: Boolean)
+
+    fun uploadCertificatesChain(chain: List<Certificate>): UploadResult {
+        val sct = httpClient.addCertificate(chain)
+        return UploadResult(sct, signatureVerifier.verifySignature(sct, chain[0]))
+    }
+
+    companion object {
+
+        @Throws(IOException::class)
+        @JvmStatic
+        fun main(args: Array<String>) {
+            if (args.size < 3) {
+                println("Usage: ${CTLogClient::class.java.simpleName} <Certificate chain> <Log URL> <Log public key> [output file]")
+                return
+            }
+
+            val pemFile = args[0]
+            val logUrl = getBaseUrl(args[1])
+            val logPublicKeyFile = args[2]
+            var outputSctFile: String? = null
+            if (args.size >= 4) {
+                outputSctFile = args[3]
+            }
+
+            val client = CTLogClient(logUrl, LogInfo.fromKeyFile(logPublicKeyFile))
+            val certs = CryptoDataLoader.certificatesFromFile(File(pemFile))
+            println(String.format("Total number of certificates: %d", certs.size))
+
+            val result = client.uploadCertificatesChain(certs)
+            if (result.isVerified) {
+                println("Upload successful ")
+                if (outputSctFile != null) {
+                    val serialized = Serializer.serializeSctToBinary(result.sct)
+                    Files.write(serialized, File(outputSctFile))
+                }
+            } else {
+                println("Log signature verification FAILED.")
+            }
+        }
+
+        private fun getBaseUrl(url: String) = "http://${url}/ct/v1/"
+    }
+}
