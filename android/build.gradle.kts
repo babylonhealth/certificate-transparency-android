c7ea4a3864f70a1439faddc7942d7392d250886f
Matthew Dolan <info@mattdolan.com>
2019-01-25 15:15:04 +0000

    Converted rest of build scripts to KTS

---
+++ android/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    compileSdkVersion(28)
+
+    defaultConfig {
+        minSdkVersion(19)
+        targetSdkVersion(28)
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+
+        consumerProguardFiles("consumer-proguard-rules.pro")
+    }
+}
+
+dependencies {
+    api(project(":lib"))
+}
---
+++ sampleapp/build.gradle.kts
@@ -0,0 +1,62 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("android.extensions")
+}
+
+android {
+    compileSdkVersion(28)
+    defaultConfig {
+        applicationId = "com.babylon.certificatetransparency.sampleapp"
+        minSdkVersion(19)
+        targetSdkVersion(28)
+        versionCode = 1
+        versionName = "1.0"
+        multiDexEnabled = true
+        proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+    compileOptions {
+        setSourceCompatibility(JavaVersion.VERSION_1_8)
+        setTargetCompatibility(JavaVersion.VERSION_1_8)
+    }
+    packagingOptions {
+        exclude("META-INF/DEPENDENCIES")
+    }
+}
+
+tasks.withType(KotlinCompile::class.java).all {
+    kotlinOptions {
+        allWarningsAsErrors = true
+    }
+}
+
+dependencies {
+    implementation(project(":android"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:${rootProject.extra["kotlin_version"]}")
+    implementation("androidx.appcompat:appcompat:1.0.2")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.0-alpha3")
+    implementation("com.google.android.material:material:1.1.0-alpha02")
+    implementation("com.google.android.gms:play-services-base:16.1.0")
+    implementation("com.squareup.retrofit2:retrofit:2.5.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.5.0")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.0.0")
+    implementation("androidx.lifecycle:lifecycle-livedata:2.0.0")
+    implementation("android.arch.navigation:navigation-fragment-ktx:1.0.0-alpha11")
+    implementation("android.arch.navigation:navigation-ui-ktx:1.0.0-alpha11")
+    implementation("com.xwray:groupie:2.3.0")
+    implementation("com.xwray:groupie-kotlin-android-extensions:2.3.0")
+    implementation("com.afollestad.material-dialogs:core:2.0.0-rc8")
+    implementation("com.afollestad.material-dialogs:input:2.0.0-rc8")
+    implementation("com.pddstudio:highlightjs-android:1.5.0")
+    implementation("com.android.volley:volley:1.1.1")
+    implementation("com.github.spullara.mustache.java:compiler:0.9.5")
+}
